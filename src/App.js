db4de1d7ad8933e15485036e9a7c09185c2f4000
karodes <sameer.karode@cdk.com>
2019-02-28 15:20:34 +0530

    Adding brogrammer logo and updating app https://github.com/users/skarode96/projects/1#card-18217881

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import brogrammer from './brogrammer-logo.png';
 
 class App extends Component {
+  
   render() {
+    const name = "<Bro/>grammers";
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <img src={brogrammer} className="App-logo"  alt="logo" />
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
+          <br/>
+          <br/>
+          <br/>
+          <p>{name}</p>
         </header>
       </div>
     );
