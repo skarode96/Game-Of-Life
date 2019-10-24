e908386c0dfad8e4f501f6deee7c0f39894b7282
skarode96 <skarode96@gmail.com>
2019-10-24 23:07:04 +0100

    Reverting to Brogrammers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import brogrammer from './brogrammer-logo.png';
 
 class App extends Component {
-  
   render() {
-    const name = "<Bro/>grammers... !! Cheers !! 123";
+    const name = "<Bro/>grammers...!!";
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={brogrammer} className="App-logo"  alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-          <br/>
-          <br/>
-          <br/>
-          <p>{name}</p>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={brogrammer} className="App-logo" alt="logo"/>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <p>{name}</p>
+            <p>Sameer hello !</p>
+          </header>
+        </div>
     );
   }
 }
